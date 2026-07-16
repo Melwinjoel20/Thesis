1c3fb2d942c4f0d0e47a0177371b3f60e123be1f
Melwinjoel20 <x24265438@student.ncirl.ie>
2026-07-16 15:09:02 +0100

    fix: health check path is /store/home/ (store app mounted under /store/ prefix)

--- terraform/modules/ElasticBeanstalk/main.tf
+++ terraform/modules/ElasticBeanstalk/main.tf
@@ -62,7 +62,7 @@ resource "aws_elastic_beanstalk_environment" "this" {
   setting {
     namespace = "aws:elasticbeanstalk:environment:process:default"
     name      = "HealthCheckPath"
-    value     = "/home/"
+    value     = "/store/home/"
   }
 
   setting {
