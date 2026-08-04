dcb82ca770637bc7923e5c48632750757d03ceac
Melwinjoel20 <x24265438@student.ncirl.ie>
2026-08-04 23:01:07 +0100

    removed the extra output

--- terraform/usecase/storage/outputs.tf
+++ terraform/usecase/storage/outputs.tf
@@ -1,6 +1,3 @@
 output "bucket_name" { value = module.s3.bucket_name }
 output "bucket_arn" { value = module.s3.bucket_arn }
 output "vpc_endpoint_id" { value = module.s3.vpc_endpoint_id }
-output "bucket_name" {
-  value = aws_s3_bucket.this.bucket
-}
